8649d8300cfaab0d67dc5168b0529442446dcf07
RedOwlMarket <evaggelossaradeas@gmail.com>
2019-11-14 15:20:50 +0200

    Added student height

--- Student.cs
+++ Student.cs
@@ -8,7 +8,7 @@ namespace Demo_Website
         public string FirstName{get;set;}
         public string LastName{get;set;}
         public DateTime BirthDate{get;set;}
-        
+        public double height {get;set;}
         
     }
 }
